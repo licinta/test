3bf9b1d926be15b967906493d3974c6b593b5497
ChenXu <3416205898@qq.com>
2022-10-29 15:49:46 +0800

    add 2 files

---
+++ test1.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: test1.cpp
+    > Author: Licinta
+    > Mail: 3416205898@qq.com
+    > Created Time: 2022-10-29
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello";
+    return 0;
+}
---
+++ test2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+ 
+# File Name: test2.py
+# Author: Licinta
+# mail: 3416205898@qq.com
+# Created Time: 2022-10-29
+
+print("hello")
